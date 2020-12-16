e77e55dfdfeba900c636c4d45212087b27fe5814
Milivojevic MIlos <milivojevicm66@gmail.com>
2020-12-16 16:05:14 +0100

    Initial Project add

---
+++ src/main/java/milos/springframework/milospetclinic/MilosPetClinicApplication.java
@@ -0,0 +1,13 @@
+package milos.springframework.milospetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilosPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MilosPetClinicApplication.class, args);
+    }
+
+}
