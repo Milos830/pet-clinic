10e04e62056b03739c24ed64d70299c52c4a2cda
Milivojevic MIlos <milivojevicm66@gmail.com>
2020-12-18 18:18:58 +0100

    Impl Map based Services. closes #6

---
+++ pet-clinic-data/src/main/java/milos/springframework/milospetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,33 @@
+package milos.springframework.milospetclinic.services.map;
+
+import milos.springframework.milospetclinic.model.Owner;
+import milos.springframework.milospetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
