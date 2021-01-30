18a1aa860b94c2ac6f43e15329cd0d76ec9a1622
Milivojevic MIlos <milivojevicm66@gmail.com>
2021-01-30 12:37:31 +0100

    Add Pet Clinic Index Page and Controller. closes #8.

---
+++ pet-clinic-web/src/main/java/milos/springframework/milospetclinic/controller/IndexController.java
@@ -0,0 +1,13 @@
+package milos.springframework.milospetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index() {
+        return  "index";
+    }
+}
