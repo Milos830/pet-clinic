88d21b1af1f854afc8207c8d3e9333c2a7765472
Milivojevic MIlos <milivojevicm66@gmail.com>
2021-01-30 13:14:18 +0100

    Editing Owner index Page and controller. closes #13

--- pet-clinic-web/src/main/java/milos/springframework/milospetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/milos/springframework/milospetclinic/controller/OwnerController.java
@@ -4,8 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/owners")
 public class OwnerController {
-    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    @RequestMapping({"","/", "/index", "/index.html"})
     public String listOwners() {
 
         return "owners/index";
