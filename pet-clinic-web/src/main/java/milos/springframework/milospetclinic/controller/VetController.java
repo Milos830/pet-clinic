d3fa60429df2bc0663b8ae14117b8285a0ce23dc
Milivojevic MIlos <milivojevicm66@gmail.com>
2021-01-30 13:01:30 +0100

    Create Vet Index Page and Controller. closes #7.

---
+++ pet-clinic-web/src/main/java/milos/springframework/milospetclinic/controller/VetController.java
@@ -0,0 +1,13 @@
+package milos.springframework.milospetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets() {
+        return "vets/index";
+    }
+}
