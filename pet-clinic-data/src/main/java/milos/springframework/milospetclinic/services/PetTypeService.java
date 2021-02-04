96cfb20d0f4eef05a7a408f59bb5b4861ddaf507
Milivojevic MIlos <milivojevicm66@gmail.com>
2021-02-04 11:28:07 +0100

    added PetTypeService closes #29

---
+++ pet-clinic-data/src/main/java/milos/springframework/milospetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package milos.springframework.milospetclinic.services;
+
+import milos.springframework.milospetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/milos/springframework/milospetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,35 @@
+package milos.springframework.milospetclinic.services.map;
+
+import milos.springframework.milospetclinic.model.PetType;
+import milos.springframework.milospetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
