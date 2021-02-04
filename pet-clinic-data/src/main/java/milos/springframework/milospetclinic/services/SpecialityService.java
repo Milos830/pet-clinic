cc9e28f19d6fcae2cc9c7d2ff902eb77dd328478
Milivojevic MIlos <milivojevicm66@gmail.com>
2021-02-04 11:33:22 +0100

    added SpecialityService and SpecialityMapService closes #31

---
+++ pet-clinic-data/src/main/java/milos/springframework/milospetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package milos.springframework.milospetclinic.services;
+
+import milos.springframework.milospetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
