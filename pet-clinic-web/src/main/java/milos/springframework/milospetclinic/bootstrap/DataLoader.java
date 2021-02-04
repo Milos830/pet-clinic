4c3dcaad19ad63658d100df51ba5a0f6d54fd265
Milivojevic MIlos <milivojevicm66@gmail.com>
2021-02-04 11:48:12 +0100

    added PetTypes to bootstrap startup. closes #32

--- pet-clinic-web/src/main/java/milos/springframework/milospetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/milos/springframework/milospetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package milos.springframework.milospetclinic.bootstrap;
 
 import milos.springframework.milospetclinic.model.Owner;
+import milos.springframework.milospetclinic.model.PetType;
 import milos.springframework.milospetclinic.model.Vet;
 import milos.springframework.milospetclinic.services.OwnerService;
+import milos.springframework.milospetclinic.services.PetTypeService;
 import milos.springframework.milospetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,15 +14,26 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
+        System.out.println("Loading PetType list ....");
 
         Owner owner1 = new Owner();
 
