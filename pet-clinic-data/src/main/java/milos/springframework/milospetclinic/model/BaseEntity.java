49813e5cc536083ab60df15b2514329dd8776a98
Milivojevic MIlos <milivojevicm66@gmail.com>
2021-02-06 11:01:28 +0100

    Update base entity with JPA anotation. closes #35

--- pet-clinic-data/src/main/java/milos/springframework/milospetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/milos/springframework/milospetclinic/model/BaseEntity.java
@@ -1,9 +1,16 @@
 package milos.springframework.milospetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass  // base class for JPA, every class will inherit from this class
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
@@ -13,4 +20,8 @@ public class BaseEntity implements Serializable {
     public void setId(Long id) {
         this.id = id;
     }
+
+//    public boolean isNew(){
+//        return  this.id == null;
+//    }
 }
