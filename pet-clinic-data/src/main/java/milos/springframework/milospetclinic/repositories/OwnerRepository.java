dda8792f34153fea1244c419bbeb10448b8c1d24
Milivojevic MIlos <milivojevicm66@gmail.com>
2021-02-06 13:34:45 +0100

    Added Spring Data JPA Service for Owners and added @Profile coz of 2 services

--- pet-clinic-data/src/main/java/milos/springframework/milospetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/milos/springframework/milospetclinic/repositories/OwnerRepository.java
@@ -3,6 +3,12 @@ package milos.springframework.milospetclinic.repositories;
 import milos.springframework.milospetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+
+
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
+
 
 }
